a29e6aed0974e009d266b10638982f4d98d53edb
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-02-02 19:55:13 +0400

    initial import - device discovery only

---
+++ src/main/java/com/github/bluebridge/EntryPoint.java
@@ -0,0 +1,71 @@
+package com.github.bluebridge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.bluetooth.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dan
+ * Date: 1/31/13
+ * Time: 10:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class);
+
+    public static void main(String[] args)
+            throws BluetoothStateException, InterruptedException {
+        logger.info("main started");
+        final Object inquiryCompletedEvent = new Object();
+
+        final List devicesDiscovered = new ArrayList();
+        devicesDiscovered.clear();
+
+        DiscoveryListener listener = new DiscoveryListener() {
+
+            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+                logger.info("Device {} found", btDevice.getBluetoothAddress());
+                devicesDiscovered.add(btDevice);
+                try {
+                    logger.info("     name {}", btDevice.getFriendlyName(false));
+                } catch (IOException cantGetDeviceName) {
+                }
+            }
+
+            public void inquiryCompleted(int discType) {
+                logger.debug("Device Inquiry completed!");
+                synchronized (inquiryCompletedEvent) {
+                    inquiryCompletedEvent.notifyAll();
+                }
+            }
+
+            public void serviceSearchCompleted(int transID, int respCode) {
+                logger.info("serviceSearchCompleted transID = {}, respCode = {}",
+                        transID, respCode);
+            }
+
+            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+                logger.info("servicesDiscovered transID = {}", transID);
+                for (ServiceRecord sr : servRecord) {
+                    logger.info("service discovered {}", sr.getAttributeIDs().length);
+                }
+            }
+        };
+
+        synchronized (inquiryCompletedEvent) {
+            boolean started = LocalDevice.getLocalDevice().
+                    getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+            if (started) {
+                logger.info("wait for device inquiry to complete...");
+                inquiryCompletedEvent.wait();
+                logger.info("{} device(s) found", devicesDiscovered.size());
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/bluebridge/LocalDeviceTest.java
@@ -0,0 +1,48 @@
+package com.github.bluebridge;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dan
+ * Date: 2/2/13
+ * Time: 7:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocalDeviceTest {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LocalDeviceTest.class);
+
+
+    @Test
+    public void testIsPowerOn() {
+        logger.info("isPowerOn {}", LocalDevice.isPowerOn());
+    }
+
+    @Test
+    public void test1() throws BluetoothStateException {
+        LocalDevice ld = LocalDevice.getLocalDevice();
+        logger.info("blue tooth address {}", ld.getBluetoothAddress());
+        logger.info("is discoverable {}", ld.getDiscoverable());
+        logger.info("friendly name {}", ld.getFriendlyName());
+        DeviceClass dc = ld.getDeviceClass();
+        logger.info("dc major device {}; minor device {}",
+                dc.getMajorDeviceClass(), dc.getMinorDeviceClass());
+        logger.info("device classes {}", dc.getServiceClasses());
+
+        DiscoveryAgent da = ld.getDiscoveryAgent();
+
+        logger.info("blue tooth address {}", ld.getBluetoothAddress());
+        logger.info("blue tooth address {}", ld.getBluetoothAddress());
+        logger.info("blue tooth address {}", ld.getBluetoothAddress());
+        logger.info("blue tooth address {}", ld.getBluetoothAddress());
+    }
+}
