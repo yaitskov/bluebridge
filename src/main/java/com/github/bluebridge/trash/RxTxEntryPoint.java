33e926ee7186514a27288aeeee88b08342adc644
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-02-15 08:25:06 +0400

    mvn profiles: rf server/client, rxtx, pclient and fake client

---
+++ src/main/java/com/github/bluebridge/trash/RxTxEntryPoint.java
@@ -0,0 +1,11 @@
+package com.github.bluebridge.trash;
+
+/**
+ * Sends data to serial port with hash checking and
+ * resending lost lines due micro controller overflows.
+ */
+public class RxTxEntryPoint {
+    public static void main(String[] args) {
+
+    }
+}
